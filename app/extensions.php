a3c06c916b3fbc76d2cec1e8d964eeb28152dfa1
Lopo <lopo@losys.sk>
2010-09-14 22:23:30 +0200

    remove unwanted extensions from extensions.php

--- app/extensions.php
+++ app/extensions.php
@@ -1,5 +1,3 @@
 <?php
 use Nette\Forms\Form;
 Form::extensionMethod('Nette\Forms\Form::addCBTree', array('CBTree', 'addCBTree'));
-Form::extensionMethod('Nette\Forms\Form::addPswdInput', array('PswdInput', 'addPswdInput'));
-Form::extensionMethod('Nette\Forms\Form::addCBox3S', array('CBox3S', 'addCBox3S'));
